f4965bcca6ef6390a4a32bc7211be20554513652
Kent Wang <yqwang@cheweilai.cn>
2019-08-29 11:27:22 +0800

    调整buffer size

--- lib/const.py
+++ lib/const.py
@@ -3,7 +3,7 @@ class Signal(object):
     控制信号
     """
     INTERRUPT_CURRENT_REQUEST = b'0'  # 中断当前请求
-    CLOSE = b'1'  # 连接关闭
+    CLOSE = b'1'  # 协商关闭连接
 
     BUFFER_RECEIVED = b'10'  # Buffer接收成功
     FILE_INFO_RECEIVED = b'11'  # 文件基本信息接收成功
@@ -13,7 +13,7 @@ class Signal(object):
     FILE_SEND_COMPLETE = b'exit'  # 文件发送完成
 
 
-DEFAULT_BUFFER_SIZE = 1024  # 默认buffer大小
+DEFAULT_BUFFER_SIZE = 8192  # 默认buffer大小
 SERVER_PORT = 10000  # 服务器默认端口
 REQUEST_TIMEOUT = 60  # 请求超时时间
 
